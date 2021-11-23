6f860ed819761acacab9dd6329868da09982fac2
Adnnann <adnanovcina1987@gmail.com>
2021-11-23 19:50:12 +0100

    React app created

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+
+import { Container, Header } from 'semantic-ui-react';
+const header = () =>{
+
+    return(
+        <Container fluid style={{backgroundColor:"red"}}/>
+    )
+}
+
+export default header;
